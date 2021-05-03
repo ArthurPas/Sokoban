c731baa3aea9b8241586d3213c67b5274c08d715
Arthur Passcal <arthur.pascal@etu.u-bordeaux.fr>
2021-05-03 10:48:33 +0200

    initial commit

---
+++ SokobanProject/src/sokobanproject/SokobanProject.java
@@ -0,0 +1,15 @@
+package sokobanproject;
+
+/**
+ *
+ * @author apascal002
+ */
+public class SokobanProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
