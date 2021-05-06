fc67cd343f6ebf6506a4cd066b03b822a7d64c44
Arthur Pascal <arthur.pascal@etu.u-bordeaux.fr>
2021-05-06 15:32:37 +0200

    Begin of KeyboardEntry class and tests

---
+++ SokobanProject/src/sokobanproject/KeyboardEntry.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sokobanproject;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author apascal002
+ */
+public class KeyboardEntry {
+    static Scanner in = new Scanner(System.in);
+    
+    /**
+     * Read the line entered by the user
+     * @return
+     */
+    public static String readLine() {
+        return in.nextLine().trim();
+    }
+    
+    public static boolean badEntry(String theEntry){
+        if(!theEntry.contains(Directions.D.toString()) || 
+                !theEntry.contains(Directions.R.toString()) ||
+                !theEntry.contains(Directions.U.toString()) ||
+                !theEntry.contains(Directions.L.toString()))
+        {
+            return false;
+        }
+        return true;
+    }
+}
+/*!theEntry.contains(Directions.R.toString())) )||(
+            !theEntry.contains(Directions.U.toString()))||(
+            !theEntry.contains(Directions.L.toString())){*/
---
+++ SokobanProject/test/sokobanproject/KeyboardEntryTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sokobanproject;
+
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import org.junit.Test;
+
+
+/**
+ *
+ * @author apascal002
+ */
+public class KeyboardEntryTest {
+    
+    /**
+     *
+     */
+    @Test
+    public void testBadEntry(){
+        String trueTest = "LURD";
+        String falseTest = "vim";
+        assertTrue(KeyboardEntry.badEntry(trueTest));
+        assertFalse(KeyboardEntry.badEntry(falseTest));
+    }
+    
+}
